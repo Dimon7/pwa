b5d86f5047bab067d959213aab948a43ce162ddb
dimon4ik7 <savchukdsma9@gmail.com>
2018-05-13 10:11:21 +0300

    pwa

--- service-worker.js
+++ service-worker.js
@@ -28,3 +28,23 @@ self.toolbox.router.any('/*', self.toolbox.cacheFirst);
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
 self.toolbox.router.default = self.toolbox.networkFirst;
+
+self.addEventListener('fetch', function(event) {
+  // … either respond with the cached object or go ahead and fetch the actual URL
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        // retrieve from cache
+        return response;
+      }
+
+      // if not found in cache, return default offline content (only if this is a navigation request)
+      if (event.request.mode === 'navigate') {
+        return caches.match('./index.html');
+      }
+
+      // fetch as normal
+      return fetch(event.request);
+    })
+  );
+});
